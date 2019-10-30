9a3390f0f3688ede8a2d9e0b2e5b97268afc3de8
imkeVr <ivanrompa@gmail.com>
2019-10-30 13:44:54 +0100

    a new start

--- CocaCola/resources/views/layouts/app.blade.php
+++ CocaCola/resources/views/layouts/app.blade.php
@@ -21,10 +21,11 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-danger shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/png/cocaColaLogo.png" style="height: 50px; border-right: 3px solid #fff" class="pr-3"></div>
+                    <div class="pl-3 pt-3 text-white"> Bottle Game - by IMKE</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- CocaCola/resources/views/welcome.blade.php
+++ CocaCola/resources/views/welcome.blade.php
@@ -65,35 +65,24 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+            <div class="content">
+                <div class="title m-b-md">
+                    Bottle Game
+                </div>
+                <div>
+                    <div><img src="/svg/cocaColahome.svg" style="height: 500px;" class="pr-3"></div>
+                </div>
+                @if (Route::has('login'))
+                <div class=" links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Register a bottle</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
                     @endauth
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
