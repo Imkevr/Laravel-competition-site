e259f2b51aa78d77d439cd62eeac0b270344129e
imkeVr <ivanrompa@gmail.com>
2019-11-01 11:59:18 +0100

    images added

--- CocaCola/resources/views/home.blade.php
+++ CocaCola/resources/views/home.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 <div class="container">
-
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/jpg/people1.jpg" class="w-100 " alt="image peopple 1">
+        </div>
+        <div class="col-4">
+            <img src="/jpg/people2.jpg" class="w-100" alt="image peopple 1">
+        </div>
+        <div class="col-4">
+            <img src="/jpg/people3.jpg" class="w-100" alt="image peopple 1">
+        </div>
+    </div>
 </div>
 @endsection
