023da117318a0454dd9476f75af11fbdda504ff6
imkeVr <ivanrompa@gmail.com>
2019-11-03 20:21:58 +0100

    default register image

--- CocaCola/app/Profile.php
+++ CocaCola/app/Profile.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = [];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : '/profile/E7X4P1Pf8gZFYEB4VrwW4tVAkzeZIXpe7V6RCJGe.png';
+        return '/storage/'. $imagePath;
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
--- CocaCola/resources/views/posts/show.blade.php
+++ CocaCola/resources/views/posts/show.blade.php
@@ -7,8 +7,23 @@
                 <img src="/storage/{{ $post->image }}" class="w-100"/>
             </div>
             <div class="col-4">
-                <h3>{{ $post->user->username}}</h3>
-                <p>{{{ $post->caption}}}</p>
+                <div>
+                    <div class="d-flex align-items-center">
+                        <div class="pr-3">
+                            <img src="{{ $post->user->profile->profileImage()}}" class="rounded-circle w-100" style="max-width: 70px;">
+                        </div>
+                        <div>
+                            <h5 class="font-weight-bold">
+                                    <a href="/profile/{{ $post->user->id}}">{{ $post->user->username}}</a>
+                            </h5>
+                        </div>
+                    </div>
+                    <hr>
+                <p class="font-weight-bold">
+                    <span class="font-weight-bold text-dark">
+                        <a href="/profile/{{ $post->user->id}}">{{ $post->user->username}}</a>
+                    </span> {{{ $post->caption}}}</p>
+                </div>
             </div>
          </div>
     </div>
